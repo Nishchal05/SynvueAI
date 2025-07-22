eef7bedc5dd9d4b0e3994f780354776806f33f3e
nishchal05 <nishchalsundan04@gmail.com>
2025-07-22 19:53:51 +0530

    update feedback flock

--- SynvueClient/app/_component/Sidebar.jsx
+++ SynvueClient/app/_component/Sidebar.jsx
@@ -87,7 +87,7 @@ const Sidebar = () => {
               Contact Us
             </div>
             <div
-              onClick={()=>{!UserButton ? router.push('/Feedback') : router.push('https://harmless-civet-15.accounts.dev/sign-up?redirect_url=http%3A%2F%2Flocalhost%3A3000%2F')}}
+              onClick={()=>{UserButton ? router.push('/Feedback') : router.push('https://harmless-civet-15.accounts.dev/sign-up?redirect_url=http%3A%2F%2Flocalhost%3A3000%2F')}}
               className="flex items-center gap-3 hover:text-indigo-600 transition cursor-pointer"
             >
              <FeedbackIcon className="text-xl bg-gradient-to-r text-indigo-700 bg-clip-text"/>
