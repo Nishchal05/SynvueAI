b882abdac014259662393ec30bb902d11b4e394b
nishchal05 <nishchalsundan04@gmail.com>
2025-08-14 21:01:44 +0530

    add sinin page

--- SynvueClient/app/signup/page.jsx
+++ SynvueClient/app/signup/page.jsx
@@ -3,7 +3,9 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+    
+    <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 p-4">
+    
       <SignUp routing="path" path="/signup" signInUrl="/signin" />
     </div>
   );
