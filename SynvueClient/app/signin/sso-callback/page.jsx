f8e32a3731bdcd52ac4b71f36d69cda14f016e1a
nishchal05 <nishchalsundan04@gmail.com>
2025-08-24 17:54:17 +0530

    add sso page

---
+++ SynvueClient/app/signin/sso-callback/page.jsx
@@ -0,0 +1,9 @@
+// app/signup/sso-callback/page.tsx
+import { Smile } from "lucide-react";
+import { redirect } from "next/navigation";
+import { toast } from "sonner";
+
+export default function SSOCallback() {
+  toast("Don’t have an account? Sign up here.",<Smile/>)
+    redirect("/signup"); 
+}
