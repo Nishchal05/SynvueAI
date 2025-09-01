0c22b8013e12ca78a66b0a68c8afecea0264adb2
nishchal05 <nishchalsundan04@gmail.com>
2025-09-01 17:10:22 +0530

    update

--- SynvueClient/app/signin/page.jsx
+++ SynvueClient/app/signin/page.jsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 p-4">
-      <SignIn routing="path" path="/signin" signUpUrl="/signup" />
+      <SignIn routing="path" path="/signin" signUpUrl="/signup" redirectUrl="/" />
     </div>
   );
 }
--- SynvueClient/app/signup/page.jsx
+++ SynvueClient/app/signup/page.jsx
@@ -6,7 +6,7 @@ export default function SignUpPage() {
     
     <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 p-4">
     
-      <SignUp routing="path" path="/signup" signInUrl="/signin" />
+      <SignUp routing="path" path="/signup" signInUrl="/signin" redirectUrl="/" />
     </div>
   );
 }
